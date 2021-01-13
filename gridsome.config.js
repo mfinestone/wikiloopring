c0769d3828adbe0d23b4c689032a4c6acad1a876
ericdrgn <60805441+ericdrgn@users.noreply.github.com>
2021-01-13 16:58:20 -0700

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Loopring Docs',
+  siteName: 'Loopring',
   icon: {
     favicon: './src/assets/favicon.png',
     touchicon: './src/assets/favicon.png'
